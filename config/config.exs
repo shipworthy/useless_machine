6ba77b21c8083d39ef147dca9e42351cc3575ee8
Mark Markaryan <markmark206@gmail.com>
2025-11-25 14:19:04 -0800

    wiring in dependencies, configuring db

---
+++ config/config.exs
@@ -0,0 +1,15 @@
+import Config
+
+config :useless_machine, ecto_repos: [Journey.Repo]
+
+config :journey, Journey.Repo,
+  database: "useless_machine",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
+config :logger,
+       :console,
+       format: "$time [$level] $metadata$message\n",
+       level: :warning,
+       metadata: [:pid, :mfa]
