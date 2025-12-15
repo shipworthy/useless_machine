ea12950bb56a387784d171647834826066f8fd90
Mark Markaryan <markmark206@gmail.com>
2025-12-14 23:38:12 -0800

    picking up journey 0.10.41 (#1)

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule UselessMachine.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:journey, "~> 0.10.40"},
+      {:journey, "~> 0.10.41"},
       {:ecto, "~> 3.12 or ~> 3.13"},
       {:postgrex, "~> 0.20 or ~> 0.21"}
     ]
